8a309744b853de3310dea86d707e170fed159ac8
Oscar Gonzalez <38286948+oscar16gonzalez@users.noreply.github.com>
2023-02-14 14:45:43 -0500

    cambios para creacion de CT con imagen

--- src/app/modals/modal-create-project/modal-create-project.component.ts
+++ src/app/modals/modal-create-project/modal-create-project.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ContractsService } from 'app/services/contract/contracts.service';
 import { UserService } from '../../services/user/users.service';
 import { FormControl } from '@angular/forms';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-modal-create-project',
@@ -14,12 +15,13 @@ export class ModalCreateProjectComponent implements OnInit {
   formCreateContract: FormGroup;
   responseData;
   listUser: any = '';
-  arrayUser ;
+  arrayUser;
+  imageCT;
 
   constructor(private fb: FormBuilder, private contract_service: ContractsService, private user_service: UserService) { }
 
   ngOnInit(): void {
-    
+
     this.createFrom();
     this.getUsers();
   }
@@ -49,11 +51,12 @@ export class ModalCreateProjectComponent implements OnInit {
       municipio: [''],
       usuarios: [],
       url_proceso: [],
-      celular_1:[''],
-      celular_2:[''],
+      celular_1: [''],
+      celular_2: [''],
       correo_1: [''],
       correo_2: [''],
       entidad: [''],
+      logo: [''],
 
     });
   }
@@ -66,42 +69,75 @@ export class ModalCreateProjectComponent implements OnInit {
     })
   }
 
+  uploadImg($event: Event) {
+    const file = ($event.target as HTMLInputElement).files[0];
+    console.log(file);
+    this.convertToBase64(file)
+
+  }
+
+  convertToBase64(file: File) {
+    const observable = new Observable((subscriber: Subscriber<any>) => {
+      this.readFile(file, subscriber)
+    });
+    observable.subscribe((data)=>{
+      console.log(data);
+      this.imageCT = data;
+      
+    })
+  }
+
+  readFile(file: File, subscriber: Subscriber<any>){
+    const filereader = new FileReader()
+
+    filereader.readAsDataURL(file);
+
+    filereader.onload=()=>{
+      subscriber.next(filereader.result);
+      subscriber.complete();
+    }
+    filereader.onerror=(error)=>{
+      subscriber.error(error)
+    }
+  }
+
 
   createProject() {
+    this.formCreateContract.value.logo = this.imageCT;
     console.log(this.formCreateContract.value);
 
-    
-     
+
+
     const idUser = JSON.parse(localStorage.getItem('infoUser'));
     this.contract_service.postProjects(this.formCreateContract.value).subscribe(data => {
       this.responseData = data;
       console.log("RESPONSE POST PRJECT", this.responseData);
-      
+
       this.updateUserProject();
     })
   }
 
-  updateUserProject(){
+  updateUserProject() {
 
     this.arrayUser = this.formCreateContract.value.usuarios;
 
     console.log("ARRAY", this.arrayUser);
     console.log("ID PROYECTO", this.responseData.proyects._id);
-    
+
     const dataContract = {
       proyectos: this.responseData.proyects._id,
       nameProyecto: this.responseData.proyects.contratista,
     }
 
     console.log("UPDATE", dataContract);
-    
+
     for (let index = 0; index < this.arrayUser.length; index++) {
       const element = this.arrayUser[index];
       console.log(element);
-      
+
       this.user_service.putProjectUser(this.arrayUser[index], dataContract).subscribe(data => {
       })
-      
+
     }
   }
 }
